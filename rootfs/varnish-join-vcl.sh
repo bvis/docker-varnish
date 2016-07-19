28a7e9a75a1a16dcc21d15a171dc8a5138ded721
Basilio Vera <basilio.vera@softonic.com>
2016-07-19 19:13:03 +0200

    Removed redundant vcl status definition

--- rootfs/varnish-join-vcl.sh
+++ rootfs/varnish-join-vcl.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-export JOINED_VCL_FILE=/tmp/all.vcl
+export JOINED_VCL_FILE=/tmp/merged.vcl
 echo "Joining configuration files"
 cat $VCL_CONFIG/*.vcl > $JOINED_VCL_FILE
+awk '{print NR, $0}' $JOINED_VCL_FILE
