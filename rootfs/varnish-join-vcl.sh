70a0112da70e2644eefae40e57e9ee877563103b
Basilio Vera <basilio.vera@softonic.com>
2018-09-04 12:40:09 +0200

    Add file name in final vcl when debug is active

--- rootfs/varnish-join-vcl.sh
+++ rootfs/varnish-join-vcl.sh
@@ -2,7 +2,8 @@
 
 export JOINED_VCL_FILE=/tmp/all.vcl
 echo "Joining configuration files"
-cat $VCL_CONFIG/*.vcl > $JOINED_VCL_FILE
+
+for file in $VCL_CONFIG/*.vcl; do echo "# $file"; cat "$file"; done | tee $JOINED_VCL_FILE
 
 if [ -n "${VARNISH_DEBUG_SECRET}" ]; then
     sed -i "s/u0AJAlRWN4/${VARNISH_DEBUG_SECRET}/g" $JOINED_VCL_FILE
